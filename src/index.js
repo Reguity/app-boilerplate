0a8ccf64e168547ac7f689139fc03cfb16d7f53e
Patrik Weibull <patrik.weibull@reguity.com>
2018-12-07 12:06:12 +0100

    WIP

--- src/index.js
+++ src/index.js
@@ -2,19 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { createProvider } from './vue-apollo'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPlus, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import mermaid from 'mermaid';
-
-[faPlus, faEdit, faTrash].forEach(icon => library.add(icon));
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 require('./assets/sass/main.scss');
-Vue.config.productionTip = false;
-Object.defineProperty(Vue.prototype, '$mermaid', { value: mermaid });
-mermaid.initialize({ startOnLoad: false });
-
-exports.createVueInstance = ({ router, name, config }) => {
+exports.createVueInstance = ({ router, name, clientId, scopes, copyright, logo }) => {
+  // TODO: config should be accessable from App!
   return new Vue({
     router,
     apolloProvider: createProvider(),
