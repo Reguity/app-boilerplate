6f9bb139c58065005861f205d47e7b05413aa518
Patrik Weibull <patrik.weibull@reguity.com>
2018-12-07 10:43:22 +0100

    Took main layout and boilerplate from website-developers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+
 import { createProvider } from './vue-apollo'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPlus, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
@@ -8,18 +8,16 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import mermaid from 'mermaid';
 
 [faPlus, faEdit, faTrash].forEach(icon => library.add(icon));
-
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
 require('./assets/sass/main.scss');
-
 Vue.config.productionTip = false;
-
 Object.defineProperty(Vue.prototype, '$mermaid', { value: mermaid });
 mermaid.initialize({ startOnLoad: false });
 
-new Vue({
-  router,
-  apolloProvider: createProvider(),
-  render: h => h(App)
-}).$mount('#app')
+exports.createVueInstance = ({ router, name, config }) => {
+  return new Vue({
+    router,
+    apolloProvider: createProvider(),
+    render: h => h(App)
+  })
+}
